06ed598ef506e419ead0b1c92798db43a9aa8dda
Alisa Schink <Alisa Schink>
2016-11-16 20:36:11 -0500

    random word selection works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,14 @@
 //var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //set catagories variable
-var catagories = ['movies', 'books', 'TV'] //array of catagories
+//var catagories = ['movies', 'books', 'TV'] //array of catagories
 //var chosenCatagory ; //selected catagory
 
 //set arrays of words for those catagories
 
-var movies = ['dawn of the dead', 'psycho', 'halloween', 'scream', 'evil dead', 'reanimator', 'nightmare on elm street', 'the conjuring', 'the texas chainsaw massacre', 'poltergeist'];
-var books = ['the shining', 'carrie', 'pet semetary', 'the exorcist', 'house of leaves', 'dracula', 'frankenstein', 'the collector', 'silence of the lambs', 'in cold blood'];
-var tv = ['the walking dead', 'american horror story', 'buffy the vampire slayer', 'ash vs evil dead', 'bates motel', 'the twilight zone', 'the x files', 'penny dreadful', 'supernatural', 'tales from the crypt'];
+//var movies = ['dawn of the dead', 'psycho', 'halloween', 'scream', 'evil dead', 'reanimator', 'nightmare on elm street', 'the conjuring', 'the texas chainsaw massacre', 'poltergeist'];
+//var books = ['the shining', 'carrie', 'pet semetary', 'the exorcist', 'house of leaves', 'dracula', 'frankenstein', 'the collector', 'silence of the lambs', 'in cold blood'];
+//var tv = ['the walking dead', 'american horror story', 'buffy the vampire slayer', 'ash vs evil dead', 'bates motel', 'the twilight zone', 'the x files', 'penny dreadful', 'supernatural', 'tales from the crypt'];
 
 //set variables for number of guesses, wins, and losses
 //var word ; //selected word
@@ -31,22 +31,39 @@ var lossesCounter = 0; //sets number of losses to 0
 		//document.getElementById("winsCounter");
 
 //allow user to select catagory
- 	//var chosenCatagory;
+ 	//var chosenCatagory
+
+//randomly select catagory and word within catagory
+var play = function(){
+	var catagories = [
+	['dawn of the dead', 'psycho', 'halloween', 'scream', 'evil dead', 'reanimator', 
+	'nightmare on elm street', 'the conjuring', 'the texas chainsaw massacre', 'poltergeist'], 
+	['the shining', 'carrie', 'pet semetary', 'the exorcist', 'house of leaves', 'dracula', 
+	'frankenstein', 'the collector', 'silence of the lambs', 'in cold blood'], ['the walking dead', 
+	'american horror story', 'buffy the vampire slayer', 'ash vs evil dead', 'bates motel', 
+	'the twilight zone', 'the x files', 'penny dreadful', 'supernatural', 'tales from the crypt'] 
+	];
+
+	var chosenCatagory = catagories[Math.floor(Math.random()*catagories.length)];
+	var chosenWord = chosenCatagory[Math.floor(Math.random()*chosenCatagory.length)];
+	console.log(chosenWord);
+}
+
+play();
+
 //if/else statements to select catagory
 
-	// var selectCatagory = function () {
-	// 	if (chosenCatagory === catagories[0]) {
-	// 		catagoryName.innerHTML = "The Chosen Catagory is Movies!";
-	// 	} else if (chosenCatagory === catagories[1]) {
-	// 		catagoryName.innerHTML = "The Chosen Catagory is Books!";
-	// 	}else if (chosenCatagory === catagories[2]) {
-	// 		catagoryName.innerHTML = "The Chosen Catagory is TV!";
-	// 	}
-	// }
+	var selectCatagory = function () {
+		if (chosenCatagory === catagories[0]) {
+			catagoryName.innerHTML = "The Chosen Catagory is Movies!";
+		} else if (chosenCatagory === catagories[1]) {
+			catagoryName.innerHTML = "The Chosen Catagory is Books!";
+		}else if (chosenCatagory === catagories[2]) {
+			catagoryName.innerHTML = "The Chosen Catagory is TV!";
+		}
+	}
 
-//randomly select word within catagory
-
-//function to record user guess and stored guessed letter
+//turn user guess into a string and stores guessed letter
 
 document.onkeyup = function(event) {
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
