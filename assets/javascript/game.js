0d6b9e7115ae231d21a881ff61017756400c0db9
Alisa Schink <Alisa Schink>
2016-11-18 03:21:22 -0500

    css started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,124 +1,146 @@
 //declare variables
-var movies = ['dawn of the dead', 'psycho', 'halloween', 'scream', 'evil dead', 're-animator', 
-	 'nightmare on elm street', 'the conjuring', 'the texas chainsaw massacre', 'poltergeist'];
-var books = ['the shining', 'carrie', 'pet semetary', 'the exorcist', 'house of leaves', 'dracula', 
-	'frankenstein', 'the collector', 'silence of the lambs', 'in cold blood'];
-var tv = ['the walking dead', 
-	 'american horror story', 'buffy the vampire slayer', 'ash vs evil dead', 'bates motel', 
-	'the twilight zone', 'the x files', 'penny dreadful', 'supernatural', 'tales from the crypt'];
+var movies = ['dawn of the dead', 'psycho', 'halloween', 'scream', 'evil dead', 're-animator',
+  'nightmare on elm street', 'the conjuring', 'the texas chainsaw massacre', 'poltergeist'
+];
+var books = ['the shining', 'carrie', 'pet semetary', 'the exorcist', 'house of leaves', 'dracula',
+  'frankenstein', 'the collector', 'silence of the lambs', 'in cold blood'
+];
+var tv = ['the walking dead',
+  'american horror story', 'buffy the vampire slayer', 'ash vs evil dead', 'bates motel',
+  'the twilight zone', 'the x files', 'penny dreadful', 'supernatural', 'tales from the crypt'
+];
 var catagories = [movies, books, tv];
 var guesses = [];
+
 var guessCounter = 13; //sets number of guesses to 13
-var chosenCatagory = catagories[Math.floor(Math.random()*catagories.length)];
-var chosenWord = chosenCatagory[Math.floor(Math.random()*chosenCatagory.length)];
+var winsCounter = 0;//sets wins to 0
+var lossesCounter = 0;//sets losses to 0
 
 
 //randomly select catagory and word within catagory
-//catagories: movies, books, and TV
-//word arrays within catagory array
 
-var play = function(){
+var play = function() {
 
-	// var chosenCatagory = catagories[Math.floor(Math.random()*catagories.length)];
-	// var chosenWord = chosenCatagory[Math.floor(Math.random()*chosenCatagory.length)];
-	console.log(chosenWord);
+  var chosenCatagory = catagories[Math.floor(Math.random() * catagories.length)];
+  var chosenWord = chosenCatagory[Math.floor(Math.random() * chosenCatagory.length)];
+  console.log(chosenWord);
 
-	//if/else statements to indicate selected catagory
-	var selectCatagory = function () {
-		if (chosenCatagory === catagories[0]) {
-			catagoryName.innerHTML = "The Chosen Catagory is Movies!";
-			
-		} else if (chosenCatagory === catagories[1]) {
-			catagoryName.innerHTML = "The Chosen Catagory is Books!";
-			
-		}else if (chosenCatagory === catagories[2]) {
-			catagoryName.innerHTML = "The Chosen Catagory is TV!";
-			
-		}
-	}
+  //if/else statements to indicate selected catagory
+  var selectCatagory = function() {
+    if (chosenCatagory === catagories[0]) {
+      catagoryName.innerHTML = "The Chosen Catagory is Movies!";
 
-	selectCatagory();
+    } else if (chosenCatagory === catagories[1]) {
+      catagoryName.innerHTML = "The Chosen Catagory is Books!";
 
-	//set _ for number of spaces in each word
-	var game = function () {
-   		var wordHolder = document.getElementById('wordSelected');
-    	var word = document.createElement('ul');
+    } else if (chosenCatagory === catagories[2]) {
+      catagoryName.innerHTML = "The Chosen Catagory is TV!";
+
+    }
+  }
+
+  selectCatagory();
+
+  //set _ for number of spaces in each word
+  var game = function() {
+    var wordHolder = document.getElementById('wordSelected');
+    var word = document.createElement('ul');
+    word.setAttribute('id', 'newWord');
 
     for (var i = 0; i < chosenWord.length; i++) {
-      		word.setAttribute('id', 'newWord');
-      		var guess = document.createElement('li');
-      		guess.setAttribute('class', 'guess');
-      	if (chosenWord[i] === "-") {
-        	guess.innerHTML = "-";
-        	var space = 1;
-      	} else if (chosenWord[i] === " ") {
-      		guess.innerHTML = " "
-      	} else {
-        	guess.innerHTML = "_";
-      	}
+      var guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (chosenWord[i] === "-") {
+        guess.innerHTML = "-";
+      } else if (chosenWord[i] === " ") {
+        guess.innerHTML = " ";
+      } else {
+        guess.innerHTML = "_";
+      }
 
-      	guesses.push(guess);
-     	 wordHolder.appendChild(word);
-      	word.appendChild(guess);
+      guesses.push(guess);
+      wordHolder.appendChild(word);
+      word.appendChild(guess);
 
-    	}
+    }
 
-    	//turn user guess into a string and stores guessed letter
+    //displays number of guesses, wins, and losses
+    var winsDiv = document.createElement("div");
+    	winsDiv.innerHTML = "Wins: " + winsCounter;
+    var lossDiv = document.createElement("div");
+    	lossDiv.innerHTML = "Losses: " + lossesCounter;
 
-	document.onkeyup = function(event) {
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		
-		//replace '_' with correct letter
-		for (var i = 0; i < chosenWord.length; i++) {
-			if (chosenWord[i] === userGuess){
-      		guesses[i].innerHTML = userGuess;
-      	}
+    scoring.appendChild(winsDiv);
+    scoring.appendChild(lossDiv);
 
-      	var j = (chosenWord.indexOf(userGuess));
-      		if (j === -1){
-      			guessCounter -= 1;
-      		};	
-    	}
-		console.log(userGuess);
-	}
 
-  	}
-
-  game();
-
-}
-
-play();
+    var guessDiv = document.createElement("div");
+    if (guessCounter < 1) {
+      guessDiv.innerHTML = "Game Over";
+      lossDiv.innerHTML = "Losses: " + lossesCounter++;
+    } else {
+      guessDiv.innerHTML = "Guesses: " + guessCounter;
+    }
+    scoring.appendChild(guessDiv);
 
 
  
 
 
-//keep user from guessing lettter already guessed
+    //turn user guess into a string and stores guessed letter
+    document.onkeyup = function(event) {
+      var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+      
 
 
+      //replace '_' with correct letter
+      for (var i = 0; i < chosenWord.length; i++) {
+        if (chosenWord[i] === userGuess) {
+          guesses[i].innerHTML = userGuess;
 
+        };
+      };
+      
 
-//displays number of guesses
+      //increases wins by 1 when word is correctly guessed
+      // var correctWord = guesses.indexOf('_');
+      // 	if(correctWord === -1){
+      // 		winsDiv.innerHTML = "Wins: " + winsCounter++;
+      		//automatically reset game
+      		//play();
+      	// };
 
-var guessDiv = document.createElement("div");
-	if (guessCounter < 1){
-		guessDiv.innerHTML = "Game Over";
-	} else {
-		guessDiv.innerHTML = "Guesses: " + guessCounter;
-	}
-	scoring.appendChild(guessDiv);
+      //creates list of guessed letters
+      var guessed = document.getElementById('guessed');
 
+      var guessedList = document.createElement('ul');
+      	guessedList.setAttribute('id', 'newGuess');
 
-//adjust number of guesses left
+      var guessedLetter = document.createElement('li');
+      	guessedLetter.setAttribute('class', 'guess');
+      	
+
+      		//only stores guessed letter if not previously guessed
+      		var newLetter = guesses.indexOf(userGuess);
+      		 if (newLetter === -1){
+      		 	guessedLetter.innerHTML = userGuess;
+      		 	guessed.appendChild(guessedList);
+      			guessedList.appendChild(guessedLetter);
+      			//reduces guesses by 1
+      			guessDiv.innerHTML = "Guesses: " + guessCounter--;
+      		};
+
+    };
+
+  };
+
+  game();
+
+};
+
+play();
+
 
 //show picture when correct word is guessed
 
 //show answer if user loses
-
- // Reset
-
-  document.getElementById('reset').onclick = function() {
-    word.parentNode.removeChild(word);
-    play();
-  };
